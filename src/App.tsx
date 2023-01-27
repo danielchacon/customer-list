d2509158643847e4d4ac6d6d0748641357ccedb7
daniel <danielchacon@gmail.com>
2023-01-27 19:35:54 +0300

    Add customer id fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 
   const addCustomer = (form: ICustomerForm) => {
     const newCustomer = {
-      id: customers.length + 1,
+      id: customers.length ? customers[customers.length - 1].id + 1 : 0,
       name: form.name,
       email: form.email,
     };
